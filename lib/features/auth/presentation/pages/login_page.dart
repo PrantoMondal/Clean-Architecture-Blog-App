2ecba918acda37a77cf0d58d1d4dbfb4ff56d4bd
KumarPranto <prantomondal517@gmail.com>
2024-07-09 02:12:43 +0600

    Login and signup ui

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,31 +1,27 @@
 import 'package:blog_app/core/theme/app_color.dart';
-import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
+import 'package:blog_app/features/auth/presentation/pages/signup_page.dart';
 import 'package:blog_app/features/auth/presentation/widgets/auth_gradient_button.dart';
 import 'package:blog_app/features/auth/presentation/widgets/auth_text_field.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class SignupPage extends StatefulWidget {
+class LoginPage extends StatefulWidget {
   static route() => MaterialPageRoute(
-        builder: (context) => const SignupPage(),
+        builder: (context) => const LoginPage(),
       );
-
-  const SignupPage({super.key});
+  const LoginPage({super.key});
 
   @override
-  State<SignupPage> createState() => _SignupPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _SignupPageState extends State<SignupPage> {
-  final nameController = TextEditingController();
+class _LoginPageState extends State<LoginPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   final formKey = GlobalKey<FormState>();
 
   @override
   void dispose() {
-    nameController.dispose();
     emailController.dispose();
     passwordController.dispose();
     // TODO: implement dispose
@@ -35,7 +31,6 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
@@ -45,18 +40,11 @@ class _SignupPageState extends State<SignupPage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const Text(
-                "Sign Up",
+                "Sign In",
                 style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
               ),
               const SizedBox(
                 height: 30,
-              ),
-              AuthTextField(
-                controller: nameController,
-                hintText: 'Name',
-              ),
-              const SizedBox(
-                height: 15,
               ),
               AuthTextField(
                 controller: emailController,
@@ -74,27 +62,26 @@ class _SignupPageState extends State<SignupPage> {
                 height: 20,
               ),
               const AuthGradientButton(
-                buttonText: "Sign Up",
+                buttonText: "Sign In",
               ),
               const SizedBox(
                 height: 20,
               ),
               RichText(
                 text: TextSpan(
-                  text: "Already have an account? ",
+                  text: "Don't have an account? ",
                   style: Theme.of(context).textTheme.titleMedium,
                   children: [
                     TextSpan(
-                      text: "Sign In",
+                      text: "Sign Up",
                       style: Theme.of(context).textTheme.titleMedium?.copyWith(
                             color: AppColor.gradient2,
                             fontWeight: FontWeight.bold,
                           ),
                       recognizer: TapGestureRecognizer()
-                        ..onTap = () => Navigator.pushAndRemoveUntil(
+                        ..onTap = () => Navigator.push(
                               context,
-                              LoginPage.route(),
-                              (route) => false,
+                              SignupPage.route(),
                             ),
                     ),
                   ],
